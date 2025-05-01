cc0ef8a640a0b510536ca93c449ade04ccd14f7c
prettieinpunk <160554112+prettieinpunk@users.noreply.github.com>
2025-05-01 16:03:30 -0400

    added animations

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("mousemove", function (e) {
+    const heart = document.createElement("div");
+    heart.className = "soft-heart";
+    heart.style.left = `${e.pageX}px`;
+    heart.style.top = `${e.pageY}px`;
+    document.body.appendChild(heart);
+  
+    setTimeout(() => {
+      heart.remove();
+    }, 800);
+});
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const nameEl = document.getElementById("fade-name");
+    nameEl.style.animation = 'fadeInFromTop 2s ease-out forwards';  // Trigger the animation
+});
